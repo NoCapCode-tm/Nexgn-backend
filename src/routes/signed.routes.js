8d7279dcd2aaec399c50c6188a710b66dfcc9747
Mohammad Ziya <ziya7376502028@gmail.com>
2026-09-16 03:01:27 +0530

    error resolved

--- src/routes/signed.routes.js
+++ src/routes/signed.routes.js
@@ -3,6 +3,7 @@ import { adminsignup, deleteAdmin, getAdmin, loginAdmin, logout } from "../contr
 import { verifyjwt } from "../middleware/auth.middleware.js";
 import { createtemplate, deletetemplate, getsingletemplate, gettemplate } from "../controller/Template.controller.js";
 import { disapprove, getrequest, getsignature, requestcancel, signrequests, statuschange, submitdoc } from "../controller/signed.controller.js";
+import { checkpermission } from "../middleware/permission.middleware.js";
 
 export const signrouter = Router();
 
@@ -15,6 +16,6 @@ signrouter.route("/getrequest/:id").get(getrequest)//secured
 signrouter.route("/getrequests").get(verifyjwt,signrequests)//already secured
 signrouter.route("/getsignature").get(verifyjwt,getsignature)//alreay scured
 signrouter.route("/reject/:id").get(disapprove)//sexured
-documentrouter.route("/requestcancel/:id").get(verifyjwt,checkpermission("Documents-Cancel Requests"),requestcancel)//secured
+signrouter.route("/requestcancel/:id").get(verifyjwt,checkpermission("Documents-Cancel Requests"),requestcancel)//secured
 
 
